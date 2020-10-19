b9dbbbdb1c6832677a0d6d9ebb44deac0cc5445c
MSCINSYSTEMS-PC\MSC IN SYSTEMS <Ruben>
2020-10-19 18:07:26 -0430

    Se creo Componente Card, configuracion basica, se importo una Libreria rxjs/operators. Y se Instalo un packcage para nombres aleatorios import * as faker from faker.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
+import { CardComponent } from './components/card/card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /*Angular Material*/
 import { MatButtonModule } from '@angular/material/button';
-import { CardComponent } from './components/card/card.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { CardComponent } from './components/card/card.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'parte20';
+
+  constructor(
+    private _http: HttpClient
+  ){}
+
+  noticias: Noticia[] = [];
+
+  ngOnInit() {
+    this._http.get('https://jsonplaceholder.typicode.com/posts').
+    pipe(
+      map((data: Noticia[]) => data.slice(0, 10))
+    )
+    .subscribe((noticias: Noticia[]) => {
+      noticias.forEach(noticia => {
+        this.noticias.push({
+          ...noticia,
+          author: faker.name.findName(),
+          url: 'https://www.noticiero.com/prueba/prueba'
+        })
+      })
+      console.log(this.noticias)
+    });
+  }
+}
+export interface Noticia{
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  author: string;
+  url: string;
 }
