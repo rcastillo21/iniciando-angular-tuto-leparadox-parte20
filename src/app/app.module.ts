80139d437ce2845315e74b9dd4ce61cc4ee5a2c8
MSCINSYSTEMS-PC\MSC IN SYSTEMS <Ruben>
2020-10-19 16:00:21 -0430

    Creacion de Componente, Instalacion de Angular y Configuracion Básica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/*Angular Material*/
+import { MatButtonModule } from '@angular/material/button';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
